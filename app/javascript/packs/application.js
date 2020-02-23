0a8a55ddbcd2d833977074ef13f07802895743a2
Anthony M <anthony.moffa@thoughtbot.com>
2020-02-23 10:13:20 -0500

    check for null timer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,26 +20,29 @@ require("channels")
 
 document.addEventListener("turbolinks:load", () => {
   const timer = document.querySelector(".timer");
-  const endTime = new Date(timer.getAttribute("data-timer-end")).getTime();
-  const timerSound = document.getElementById("timer_alert")
-  timerSound.volume = 0.6;
 
-  const interval = setInterval(()=>{
-    const now = new Date().getTime()
-    const distance = endTime - now;
-    // const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  if (timer) {
+    const endTime = new Date(timer.getAttribute("data-timer-end")).getTime();
+    const timerSound = document.getElementById("timer_alert")
+    timerSound.volume = 0.6;
 
-    timer.innerHTML = `${minutes}m ${seconds}s`;
+    const interval = setInterval(()=>{
+      const now = new Date().getTime()
+      const distance = endTime - now;
+      // const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    if (distance >= 0 && distance <= 500 && timerSound.paused) {
-      timerSound.play();
-    }
+      timer.innerHTML = `${minutes}m ${seconds}s`;
 
-    if (distance < 0) {
-      clearInterval(interval);
-      timer.innerHTML = "TIMES UP";
-    }
-  })
+      if (distance >= 0 && distance <= 500 && timerSound.paused) {
+        timerSound.play();
+      }
+
+      if (distance < 0) {
+        clearInterval(interval);
+        timer.innerHTML = "TIMES UP";
+      }
+    })
+  }
 })
