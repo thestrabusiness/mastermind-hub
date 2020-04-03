7744e51253ff084f9dbb7604aa983917eab6b70a
Anthony M <anthony.moffa@thoughtbot.com>
2020-04-02 21:32:04 -0400

    Use heroku-compatible path to mailer previews

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.preview_path = "#{Rails.root}/mailers/previews"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
