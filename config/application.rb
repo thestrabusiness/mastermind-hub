c27e50592fc3c93840a9cb726dddbdfe6a869f01
Anthony M <anthony.moffa@thoughtbot.com>
2020-04-02 21:53:31 -0400

    Use default rails path for mailer previews

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ Bundler.require(*Rails.groups)
 module MastermindHub
   class Application < Rails::Application
     config.load_defaults 6.0
-    config.action_mailer.preview_path = "#{Rails.root}/app/mailers/previews"
     config.action_mailer.default_url_options = { host: ENV.fetch('APPLICATION_HOST') }
   end
 end
