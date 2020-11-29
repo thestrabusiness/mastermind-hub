666ee1a0536f8d159311e72948de296cecdf1336
Anthony M <anthony.moffa@thoughtbot.com>
2020-11-28 19:05:14 -0500

    forgot to add spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe User do
+  describe "validations" do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+  end
+end
