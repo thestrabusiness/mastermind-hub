f9b71c6d603dce1273b89c89047f59574aeba61b
Anthony M <anthony.moffa@thoughtbot.com>
2020-12-07 21:38:43 -0500

    Add basic styling to user profile page
    This commit adds some nicer styling to the user profile page, including
    adding the user's avatar to the page.
    
    We also refactored the `UserAvatar` class to accept a size argument so a
    larger-size image can be used where desired. These changes include some
    clean up of the tests for the `UserAvatar` class.
    
    Co-authored-by: Kurt G. <soad1789@gmail.com>

--- spec/renderers/user_avatar_spec.rb
+++ spec/renderers/user_avatar_spec.rb
@@ -3,27 +3,95 @@
 require "rails_helper"
 
 RSpec.describe UserAvatar do
-  describe "render" do
+  describe ".new" do
+    it "raises if the given size is not defined in AVATAR_SIZES" do
+      user = build(:user)
+
+      expect { UserAvatar.new(user, size: "bananas") }
+        .to raise_error UserAvatar::SizeNotDefined
+    end
+  end
+
+  describe ".render" do
     context "when the user has a gravatar" do
       it "renders an image with the gravatar src" do
-        response = Net::HTTPSuccess.new("1.1", 200, "")
-        allow(Net::HTTP).to receive(:get_response).and_return(response)
+        stub_http_request success_response
+        user = build(:user)
+        image_url = build_gravatar_url(user)
 
-        user = create(:user)
-        email_hash = Digest::MD5.hexdigest(user.email)
-        image_src = "https://www.gravatar.com/avatar/#{email_hash}"
+        result = UserAvatar.render(user)
 
-        expect(UserAvatar.render(user))
-          .to eq "<img class=\"avatar\" src=\"#{image_src}\" />"
-      end
-
-      it "renders the user's initials when the user doesn't have a gravatar" do
-        response = Net::HTTPNotFound.new("1.1", 404, "")
-        allow(Net::HTTP).to receive(:get_response).and_return(response)
-        user = create(:user)
-
-        expect(UserAvatar.render(user)).to eq "<div class=\"avatar\">EM</div>"
+        expect(result).to eq "<img class=\"avatar\" src=\"#{image_url}\" />"
       end
     end
+
+    it "renders the user's initials when the user doesn't have a gravatar" do
+      stub_http_request error_response
+      user = build(:user)
+
+      result = UserAvatar.render(user)
+
+      expect(result).to eq "<div class=\"avatar\">EM</div>"
+    end
+
+    context "when the size is small" do
+      it "uses 'avatar' class" do
+        response = Net::HTTPSuccess.new("1.1", 200, "")
+        stub_http_request(response)
+        user = build(:user)
+
+        result = UserAvatar.render(user, size: :small)
+
+        expect(result).to include "class=\"avatar\""
+      end
+
+      it "doesn't add a size query param to the image source" do
+        stub_http_request success_response
+        user = build(:user)
+
+        result = UserAvatar.render(user, size: :small)
+
+        expect(result).to_not include "?s="
+      end
+    end
+
+    context "when the size is large" do
+      it "uses 'avatar-large' class" do
+        stub_http_request success_response
+        user = build(:user)
+
+        user_avatar = UserAvatar.render(user, size: :large)
+
+        expect(user_avatar).to include "class=\"avatar avatar-large\""
+      end
+
+      it "adds a size query param image source" do
+        stub_http_request success_response
+        user = build(:user)
+        image_url = build_gravatar_url(user)
+        gravatar_size = UserAvatar::LARGE_SIZE
+
+        result = UserAvatar.render(user, size: :large)
+
+        expect(result).to include "src=\"#{image_url}?s=#{gravatar_size}\""
+      end
+    end
+  end
+
+  def stub_http_request(response)
+    allow(Net::HTTP).to receive(:get_response).and_return(response)
+  end
+
+  def build_gravatar_url(user)
+    email_hash = Digest::MD5.hexdigest(user.email)
+    "https://www.gravatar.com/avatar/#{email_hash}"
+  end
+
+  def success_response
+    Net::HTTPSuccess.new("1.1", 200, "")
+  end
+
+  def error_response
+    Net::HTTPNotFound.new("1.1", 404, "")
   end
 end
