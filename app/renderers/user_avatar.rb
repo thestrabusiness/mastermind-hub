65cfaef3af11ede3383efe6d289594514f924076
Anthony M <thestrabusiness@users.noreply.github.com>
2020-12-12 09:08:02 -0500

    Add basic styling to user profile page (#61)
    This commit adds some nicer styling to the user profile page, including
    adding the user's avatar to the page.
    
    We also refactored the `UserAvatar` class to accept a size argument so a
    larger-size image can be used where desired. These changes include some
    clean up of the tests for the `UserAvatar` class.
    
    Co-authored-by: Kurt G. <soad1789@gmail.com>

--- app/renderers/user_avatar.rb
+++ app/renderers/user_avatar.rb
@@ -4,32 +4,53 @@ class UserAvatar
   include ActionView::Helpers::AssetTagHelper
   include ActionView::Helpers::TagHelper
 
-  def initialize(user)
+  class SizeNotDefined < StandardError; end
+
+  SIZES = [:small, :large].freeze
+  LARGE_PIXEL_SIZE = 250
+
+  AVATAR_PROPS = {
+    small: OpenStruct.new(css_class: nil, gravatar_size: nil),
+    large: OpenStruct.new(css_class: " avatar-large",
+                          gravatar_size: LARGE_PIXEL_SIZE)
+  }.freeze
+
+  def initialize(user, size)
+    unless SIZES.include?(size)
+      raise SizeNotDefined, "Size must be included in AVATAR_SIZES"
+    end
+
     @user = user
+    @avatar_props = AVATAR_PROPS[size]
   end
 
-  def self.render(user)
-    new(user).render
+  def self.render(user, size: :small)
+    new(user, size).render
   end
 
   def render
     if user_has_gravatar?
-      image_tag(image_src, class: "avatar")
+      image_tag(image_src, class: "avatar#{avatar_props.css_class}")
     else
-      content_tag(:div, user.initials, class: "avatar")
+      content_tag(:div, user.initials,
+                  class: "avatar#{avatar_props.css_class}")
     end
   end
 
   private
 
-  attr_reader :user
+  attr_reader :avatar_props, :user
 
   def email_hash
     Digest::MD5.hexdigest(user.email)
   end
 
   def image_src
-    "https://www.gravatar.com/avatar/#{email_hash}"
+    "https://www.gravatar.com/avatar/#{email_hash}#{size_param}"
+  end
+
+  def size_param
+    "?s=#{avatar_props.gravatar_size}" unless avatar_props.gravatar_size.nil?
   end
 
   def user_has_gravatar?
