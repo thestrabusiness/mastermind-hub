b9afaea1ab8b80a2c265e087db3c5f695c8e8cc7
Anthony M <thestrabusiness@users.noreply.github.com>
2021-06-07 14:33:54 -0400

    Remove automated call creation logic (#75)
    This commit drops some flakey automated call creation logic in favor of manually creating calls.
    
    When there isn't an upcoming call scheduled, the user will be presented with a link to schedule a new call.

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -2,11 +2,27 @@
 
 class CallsController < ApplicationController
   before_action :require_login
-  before_action :load_and_authorize_call
+  before_action :load_and_authorize_call, except: [:new, :create]
 
   def show
     call = Call.includes(commitments: :membership).find(params[:id])
     @page = CallPage.new(call, current_user)
+  end
+
+  def new
+    @group = Group.find(params[:group_id])
+    @call = Call.new(scheduled_on: @group.next_call_date)
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @call = Call.new(call_params.merge(group: @group))
+
+    if @call.save
+      redirect_to call_path(@call)
+    else
+      render :new
+    end
   end
 
   def update
@@ -20,9 +36,16 @@ class CallsController < ApplicationController
   private
 
   def load_and_authorize_call
-    @call = Call.find(params[:id])
+    action = action_name.to_sym
+    if [:show, :update].include? action
+      @call = Call.find(params[:id])
 
-    redirect_to groups_path unless @call.group.users.includes(current_user)
+      redirect_to groups_path unless @call.group.users.includes(current_user)
+    end
+
+    if [:new, :create].include? action
+      redirect_to groups_path unless @group.users.includes(current_user)
+    end
   end
 
   def call_params
